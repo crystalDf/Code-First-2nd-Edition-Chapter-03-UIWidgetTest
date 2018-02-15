7d3c1c53fbb872e98e95a2d066652361abe8aeca
crystalDf <chendong333@gmail.com>
2018-02-15 13:24:47 +0800

    Chapter 03 UIWidgetTest

--- app/src/main/java/com/star/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/star/uiwidgettest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.star.uiwidgettest;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -24,58 +23,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = (Button) findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputText = mEditText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_LONG).show();
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(v -> {
+            String inputText = mEditText.getText().toString();
+            Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_LONG).show();
 
-                mImageView.setImageResource(R.drawable.emma2);
+            mImageView.setImageResource(R.drawable.emma2);
 
-                if (mProgressBar.getVisibility() == View.GONE) {
-                    mProgressBar.setVisibility(View.VISIBLE);
-                } else {
-                    mProgressBar.setVisibility(View.GONE);
-                }
-
-                int progress = mProgressBar.getProgress();
-                if (progress < mProgressBar.getMax()) {
-                    progress += 10;
-                    mProgressBar.setProgress(progress);
-                }
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                        .setTitle("AlertDialog")
-                        .setMessage("Something important")
-                        .setCancelable(false)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        })
-                        .show();
-
-                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-                progressDialog.setTitle("ProgressDialog");
-                progressDialog.setMessage("Loading...");
-                progressDialog.setCancelable(true);
-                progressDialog.show();
+            if (mProgressBar.getVisibility() == View.GONE) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            } else {
+                mProgressBar.setVisibility(View.GONE);
             }
+
+            int progress = mProgressBar.getProgress();
+            if (progress < mProgressBar.getMax()) {
+                progress += 10;
+                mProgressBar.setProgress(progress);
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder
+                    .setTitle("AlertDialog")
+                    .setMessage("Something important")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", (dialog, which) -> {
+
+                    })
+                    .setNegativeButton("Cancel", (dialog, which) -> {
+
+                    })
+                    .show();
+
+            ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("ProgressDialog");
+            progressDialog.setMessage("Loading...");
+            progressDialog.setCancelable(true);
+            progressDialog.show();
         });
 
-        mEditText = (EditText) findViewById(R.id.edit_text);
+        mEditText = findViewById(R.id.edit_text);
 
-        mImageView = (ImageView) findViewById(R.id.image_view);
+        mImageView = findViewById(R.id.image_view);
 
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mProgressBar = findViewById(R.id.progress_bar);
     }
 }
